1dcb918fefb33e1141d7e0aeb2509c3808a9631c
soulofjava <isamaulanatantra@gmail.com>
2023-01-04 10:38:34 +0700

    api is done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -61,4 +61,23 @@ class AuthController extends Controller
             'user' => auth()->user()
         ], 200);
     }
+
+    public function update(Request $request)
+    {
+        $attrs = $request->validate([
+            'name' => 'required|string',
+        ]);
+
+        $image = $this->saveImage($request->image, 'post');
+
+        auth()->user()->update([
+            'name' => $attrs['name'],
+            'image' => $image
+        ]);
+
+        return response([
+            'message' => 'User updated.',
+            'user' => auth()->user()
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,10 +22,30 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
+// public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    // user
     Route::get('/user', [AuthController::class, 'user']);
+    Route::put('/user', [AuthController::class, 'update']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // post
+    Route::get('/posts', [PostController::class, 'index']); // all post
+    Route::post('/posts', [PostController::class, 'store']); // create post
+    Route::get('/posts/{id}', [PostController::class, 'show']); // get single post
+    Route::put('/posts/{id}', [PostController::class, 'update']); // update post
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']); // delete post
+
+    // comment
+    Route::get('/posts/{id}/comments', [CommentController::class, 'index']); // all comment of a post
+    Route::post('/posts/{id}/comments', [CommentController::class, 'store']); // create comment on a post
+    Route::put('/comments/{id}', [CommentController::class, 'update']); // update comment
+    Route::delete('/comments/{id}', [CommentController::class, 'destroy']); // delete a comment
+
+    //like
+    Route::post('/posts/{id}/likes', [LikeController::class, 'likeOrUnlike']); // like or unlike a post
 });
